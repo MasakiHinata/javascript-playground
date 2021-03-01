bd1032863e45550df87d54ec004edf95766bae97
ishizakamasaki <robomasaki2@gmail.com>
2021-03-01 12:16:49 +0900

    feat: list item query

--- react/style/src/StyledComponent.js
+++ react/style/src/StyledComponent.js
@@ -15,9 +15,30 @@ const ChildStyledText = styled(StyledText)`
     font-weight: bold;
 `
 
+const StyledItem = styled.div`
+    width: 300px;
+    height: 100px;
+    background-color: green;
+`
+
+const StyledList = styled.div`    
+    & ${StyledItem}:first-child {
+        margin-top: 0px;
+    }
+
+    & ${StyledItem} {
+        margin-top: 16px;
+    }
+`
+
 export const StyledComponent = () => {
     return (<div>
         <StyledText fontSize={30}>Styled Component</StyledText>
         <ChildStyledText fontSize={30}>Styled Component</ChildStyledText>
+        <StyledList>
+            <StyledItem />
+            <StyledItem />
+            <StyledItem />
+        </StyledList>
     </div>)
 }
